ac2a65727ca52701f5b7bd89ce9a0e919e5ae6f0
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-19 11:15:44 +0300

    init class DatabaseHelper

---
+++ booking-catalog/core/models/db_helper.py
@@ -0,0 +1,34 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str) -> None:
+        self.engine = create_async_engine(
+            url,
+            echo=settings.db.echo,
+        )
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=settings.db.url,
+)
