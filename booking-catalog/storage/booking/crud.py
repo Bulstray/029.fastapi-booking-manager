4aa8e4e8e49ec2844edaab6e88b5b38df3534b73
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-19 15:19:16 +0300

    update service type

--- booking-catalog/storage/booking/crud.py
+++ booking-catalog/storage/booking/crud.py
@@ -1,7 +1,9 @@
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Booking
 from core.schemas import BookingCreate
+from core.types import ServiceType
 
 
 async def get_booking_by_id(
@@ -17,4 +19,14 @@ async def create_booking(
 ) -> None:
     booking = Booking(**booking_in.model_dump())
     session.add(booking)
+    await session.commit()
+
+
+async def update_service_type(
+    session: AsyncSession,
+    id_: int,
+    service_type: ServiceType,
+) -> None:
+    stmt = update(Booking).where(Booking.id == id_).values(service_type=service_type)
+    await session.execute(stmt)
     await session.commit()
