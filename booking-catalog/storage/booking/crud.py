5b620f0f65780ae69f3a2c571c8290e626ba33cd
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-19 13:17:33 +0300

    create booking

--- booking-catalog/storage/booking/crud.py
+++ booking-catalog/storage/booking/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Booking
+from core.schemas import BookingCreate
 
 
 async def get_booking_by_id(
@@ -8,3 +9,12 @@ async def get_booking_by_id(
     id_: int,
 ) -> Booking | None:
     return await session.get(Booking, id_)
+
+
+async def create_booking(
+    session: AsyncSession,
+    booking_in: BookingCreate,
+) -> None:
+    booking = Booking(**booking_in.model_dump())
+    session.add(booking)
+    await session.commit()
