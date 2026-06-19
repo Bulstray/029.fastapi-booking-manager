4252f64963dc0506c6934491c5a4da1fe2dab88e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-19 14:27:30 +0300

    create taskiq broker

--- booking-catalog/core/taskiq_broker.py
+++ booking-catalog/core/taskiq_broker.py
@@ -1,15 +1,19 @@
 import logging
 
 from taskiq import TaskiqEvents, TaskiqState
-from taskiq_aio_pika import AioPikaBroker
+from taskiq_redis import RedisAsyncResultBackend, RedisStreamBroker
 
 from core.config import settings
 
 log = logging.getLogger(__name__)
 
-broker = AioPikaBroker(
-    url=f"{settings.broker.url}",
+result_backend = RedisAsyncResultBackend(
+    redis_url=settings.broker.url,
 )
+
+broker = RedisStreamBroker(
+    url=settings.broker.url,
+).with_result_backend(result_backend)
 
 
 @broker.on_event(TaskiqEvents.WORKER_STARTUP)
