3e92f659ea3d5752d3424f424f991dff59c6d328
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-19 12:30:48 +0300

    create schemas

---
+++ booking-catalog/core/schemas/booking.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from core.types import ServiceType
+
+
+class BookingBase(BaseModel):
+    name: str
+    datetime: datetime
+
+
+class BookingRead(BaseModel):
+    service_type: ServiceType
+
+
+class BookingReadList(BaseModel, BookingRead):
+    """Модель списка всех записей"""
+
+
+class BookingCreate(BookingBase):
+    """Модель для создания брони"""
