bf0ab4967089d1e939b934c8b3df78dbab9ae524
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-19 11:03:51 +0300

    create model booking

---
+++ booking-catalog/core/models/booking.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.types import ServiceType
+
+from .base import Base
+from .mixin import IntIdPkMixin
+
+
+class Booking(IntIdPkMixin, Base):
+    __tablename__ = "bookings"
+
+    name: Mapped[str]
+    reservation_date: Mapped[datetime]
+    service_type: Mapped[ServiceType] = mapped_column(
+        Enum(ServiceType),
+        default=ServiceType.pending,
+        server_default=ServiceType.pending,
+    )
