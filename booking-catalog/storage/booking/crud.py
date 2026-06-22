2face603fab59c968eaff668fd70701b55db03f1
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-22 15:51:37 +0300

    add delete and get bookings

--- booking-catalog/storage/booking/crud.py
+++ booking-catalog/storage/booking/crud.py
@@ -1,4 +1,4 @@
-from sqlalchemy import update
+from sqlalchemy import update, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Booking
@@ -16,10 +16,11 @@ async def get_booking_by_id(
 async def create_booking(
     session: AsyncSession,
     booking_in: BookingCreate,
-) -> None:
+) -> Booking:
     booking = Booking(**booking_in.model_dump())
     session.add(booking)
     await session.commit()
+    return booking
 
 
 async def update_service_type(
@@ -30,3 +31,29 @@ async def update_service_type(
     stmt = update(Booking).where(Booking.id == id_).values(service_type=service_type)
     await session.execute(stmt)
     await session.commit()
+
+
+async def delete_booking(session, booking: Booking) -> None:
+    await session.delete(booking)
+    await session.commit()
+
+
+async def get_bookings(
+    session: AsyncSession,
+    service_type: str | None = None,
+    page: int = 1,
+    size: int = 10,
+) -> list[Booking]:
+    """Возвращает список броней с пагинацией и общее количество."""
+    stmt = select(Booking)
+
+    if service_type:
+        stmt = stmt.where(Booking.service_type == service_type)
+
+    offset = (page - 1) * size
+    stmt = stmt.offset(offset).limit(size)
+
+    result = await session.execute(stmt)
+    bookings = result.scalars().all()
+
+    return bookings
