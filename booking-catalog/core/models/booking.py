1561473cf02217feff6d42c6bc5cd624bb3496e0
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-22 15:50:42 +0300

    add timezone

--- booking-catalog/core/models/booking.py
+++ booking-catalog/core/models/booking.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import Enum
+from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.types import ServiceType
@@ -13,7 +14,9 @@ class Booking(IntIdPkMixin, Base):
     __tablename__ = "bookings"
 
     name: Mapped[str]
-    datetime: Mapped[datetime]
+    datetime: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+    )
     service_type: Mapped[ServiceType] = mapped_column(
         Enum(ServiceType),
         default=ServiceType.pending,
