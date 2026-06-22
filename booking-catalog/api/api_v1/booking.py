b26549716ed5cb600a7e2bbf46f21b21d6791d2e
Bulstray <96692002+Bulstray@users.noreply.github.com>
2026-06-22 11:21:06 +0300

    add endpoint delete

--- booking-catalog/api/api_v1/booking.py
+++ booking-catalog/api/api_v1/booking.py
@@ -8,6 +8,9 @@ from core.models import Booking as BookingModel
 from core.models import db_helper
 from core.schemas import BookingCreate, BookingRead
 from storage.booking import crud as booking_crud
+from core.types import ServiceType
+
+from tasks import update_service_type
 
 router = APIRouter(
     prefix=settings.api.v1.bookings,
@@ -25,7 +28,6 @@ async def get_booking_by_id(
         Depends(db_helper.session_getter),
     ],
 ) -> BookingModel:
-
     if booking := await booking_crud.get_booking_by_id(session, id_):
         return booking
 
@@ -43,7 +45,36 @@ async def create_booking(
         Depends(db_helper.session_getter),
     ],
 ) -> dict[str, str]:
-    await booking_crud.create_booking(session, booking_in)
+    booking = await booking_crud.create_booking(session, booking_in)
+    await update_service_type.kiq(booking.id)
     return {
         "message": "Booking is publisher",
     }
+
+
+@router.delete(
+    "/{id_}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_booking(
+    id_: int,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+) -> None:
+    booking = await booking_crud.get_booking_by_id(session, id_)
+
+    if not booking:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Booking not found",
+        )
+
+    if booking.service_type != ServiceType.pending:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Cannot delete booking: only bookings with 'pending' status can be deleted",
+        )
+
+    await booking_crud.delete_booking(session, booking)
